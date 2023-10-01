300677794c817116b63fa8e73472bf6ea1b2d9a0
Kaleb <dassou-ablam-kaleb.sika@etu-univ-cotedazur.fr>
2023-10-01 13:13:38 +0200

    Add of VotingSystems

--- TsLongLabServer/src/VotingSystems/VotingSystem.java
+++ TsLongLabServer/src/VotingSystems/VotingSystem.java
@@ -1,4 +1,58 @@
 package VotingSystems;
 
-public class VotingSystem {
+import Users.Candidates.Candidate;
+import Users.Voters.Voter;
+import Users.Voters.VoterManager;
+import Votes.VoteManager;
+import VotingBallots.VotingBallot;
+import VotingBallots.VotingBallotManager;
+import commonInterfaces.IVotingSystem;
+
+import java.util.*;
+
+public class VotingSystem implements IVotingSystem {
+    private VoterManager voterManager;
+    private VotingBallotManager ballotManager;
+    private VoteManager votesManager;
+
+    public VotingSystem() {
+        voterManager = VoterManager.getInstance();
+        ballotManager = VotingBallotManager.getInstance();
+        votesManager = VoteManager.getInstance();
+    }
+
+    // Faire voter tous les électeurs
+    public void conductVoting() {
+        Set<Voter> allVoters = voterManager.getAllVoters();
+
+        for (Voter voter : allVoters) {
+            if (!ballotManager.hasVoted(voter)) {
+                VotingBallot ballot = voter.castVotes();
+                ballotManager.submitBallot(ballot);
+            }
+        }
+    }
+
+    // Récupérer le total des votes par candidat
+    public Map<Candidate, Integer> getTotalVotesByCandidate() {
+        return votesManager.getVotesByCandidate();
+    }
+
+    // Afficher les candidats par ordre de votes décroissants
+    public void displayResults() {
+        Map<Candidate, Integer> results = getTotalVotesByCandidate();
+
+        // Trier les résultats
+        List<Map.Entry<Candidate, Integer>> sortedResults = new ArrayList<>(results.entrySet());
+        sortedResults.sort(Map.Entry.<Candidate, Integer>comparingByValue().reversed());
+
+        // Afficher les résultats
+        for (Map.Entry<Candidate, Integer> entry : sortedResults) {
+            System.out.println(entry.getKey().getName() + ": " + entry.getValue() + " votes");
+        }
+    }
 }
+
+// Note : J'ai supposé que vous avez une classe VotesManager qui a la méthode getVotesByCandidate.
+// Assurez-vous d'intégrer tous les éléments nécessaires pour que cette classe fonctionne.
+
---
+++ TsLongLabServer/src/commonInterfaces/IVotingSystem.java
@@ -0,0 +1,4 @@
+package commonInterfaces;
+
+public interface IVotingSystem {
+}
